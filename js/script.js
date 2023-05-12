8bdf0c86e1aa1e27e4bc19d76de1aebf17cc42fe
Taylor Behrens <126136711+taylorbeee@users.noreply.github.com>
2023-05-12 11:19:44 -0700

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,30 @@
+var fans = document.querySelector(".fans");
+var fish = document.querySelector(".fish");
+var pets = document.querySelector(".pets");
+
+fans.addEventListener("mouseover", function () {
+  fans.innerText = "123K";
+});
+fish.addEventListener("mouseover", function () {
+  fish.innerText = "47K";
+});
+pets.addEventListener("mouseover", function () {
+  pets.innerText = "20K";
+});
+
+var toggle = document.querySelector("#toggle");
+var ball = document.querySelector(".ball");
+var quincy = document.querySelector("h1");
+var body = document.querySelector("body");
+
+toggle.addEventListener("click", function () {
+  if (body.classList.contains("light")) {
+    body.classList.remove("light");
+    ball.classList.remove("move-right");
+    quincy.innerText = "Stealth Quincy";
+  } else {
+    body.classList.add("light");
+    ball.classList.add("move-right");
+    quincy.innerText = "Party Quincy";
+  }
+});
